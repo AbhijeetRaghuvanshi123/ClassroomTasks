0344b15735494f571b37fcb355b20ae713174166
AbhijeetRaghuvanshi123 <abhijeetraghuvanshi9842abhi@gmail.com>
2025-11-20 17:07:20 +0530

    mini project

--- JavaScript/functionborrowing.js
+++ JavaScript/functionborrowing.js
@@ -1,8 +1,9 @@
 let obj = {
-    name : 'abhijeet',
-    lname : 'baliyan',
-    getName: function (){ //cant use arrow function for this keywordfjldkjs
-        console.log(`my name is ${this.name} ${this.lname}`);
-    },
-}
+  name: "abhijeet",
+  lname: "baliyan",
+  getName: function () {
+    //cant use arrow function for this keyword
+    console.log(`my name is ${this.name} ${this.lname}`);
+  },
+};
 obj.getName();
